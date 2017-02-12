362896842826d85d057c7a20bb8b270a8f24db93
Atlas <Atlas@ATLAS-PC>
2017-02-12 17:40:31 -0500

    Title Screen Created

--- BnD Cpp/Main.cpp
+++ BnD Cpp/Main.cpp
@@ -1,12 +1,39 @@
 //Streams
 #include<iostream>
-#include<sstream>
+//#include<sstream>
 //#include<fstream>
 
 //Essentials
-#include <string>
+//#include <string>
+
+inline void Clear()
+{
+	system("cls");
+}
 
 int main()
 {
+	while (true)
+	{
+		Clear();
+		std::cout << "\n\nBattles and Dungeons: C++ Edition\n" << std::endl;
+		std::cout << "\t[P]lay Game\n\t[L]oad Game\n\t[O]ptions\n\t[E]xit\n" << std::endl;
+		char input;
+		std::cin >> input;
+		switch (input)
+		{
+		case('P'):
+			//RunGame(CreateGame());
+			break;
+		case('L'):
+			//RunGame(LoadGame());
+			break;
+		case('O'):
+			//Options();
+			break;
+		case('E'):
+			exit(0);
+		}
+	}
 	return 0;
 }
